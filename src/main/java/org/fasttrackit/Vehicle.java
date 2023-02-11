b6e8b08365010aadbaf39f974e19cf27e61b5668
Georgian Olt <olt_georgian@yahoo.com>
2023-02-11 14:21:56 +0200

    conditional statement

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -15,7 +15,21 @@ public class Vehicle {
     String color;
 
     public double accelerate(double speed, double durationInHours) {
-         // Ferrari is accelerating with 100km/h for 1 hour.
+        if (fuelLevel <= 0 || damaged) {
+            System.out.println("You cannot accelerate.");
+            return 0;
+        }
+
+
+        if (speed > maxSpeed) {
+            System.out.println("Max speed exceeded.");
+            return 0;
+        }else if (speed == maxSpeed) {
+            System.out.println("Careful, max speed reached.");
+        }
+        else {
+            System.out.println("Valid speed entered.");
+        }
         System.out.println(name + "is accelerating with" + speed + " km/h for" + durationInHours + "h");
 // Local variable
         double distance = speed * durationInHours;
@@ -25,7 +39,14 @@ public class Vehicle {
         // same result as statement above
       //  totalTraveledDistance += distance;
         System.out.println("Total traveled distance for vehicle" + name + ": " + totalTraveledDistance);
-        double usedFuel = distance * mileage / 100;
+
+        double mileageMultiplier = 1;
+
+        if (speed > 120) {
+            mileageMultiplier = speed / 100;
+        }
+
+        double usedFuel = distance * mileage * mileageMultiplier / 100;
         // fuelLevel = fuelLevel - usedFuel;
         // same statement as above
         fuelLevel -= usedFuel;
